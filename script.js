649fc4252473f2cd26371d61881ae180180ff6d4
AkinyiOwuor <owuorrachel@gmail.com>
2020-02-15 19:37:33 +0300

    add an array

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// var submit = document.getElementById("submit");
-// submit.addEventListener("click", function() {
-//     var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//     var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-//     var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-//     var dd = document.getElementById("day").value;
-//     var mm = document.getElementById("month").value;
+var submit = document.getElementById("submit");
+submit.addEventListener("click", function() {
+    var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var dd = document.getElementById("day").value;
+    var mm = document.getElementById("month").value;
 //     var yy = document.getElementById("year").value;
 //     var date = new Date(yy + "-" + mm + "-" + dd);
 //     var daysOfWeek = date.getDay();
@@ -19,8 +19,8 @@
 //     } else if (yy.toString().length !== 4) {
 //         alert("invalid year");
 //     } else if (male.checked) {
-//         output.innerHTML = "Hey!" + name + ". Your Akan Name Is " + maleAkanNames[daysOfWeek];
+//         output.innerHTML = "Hello!" + name + ". Your Akan Name Is " + maleAkanNames[daysOfWeek];
 //     } else {
-//         output.innerHTML = "Hey!" + name + ". Your Akan Name Is " + femaleAkanNames[daysOfWeek];
+//         output.innerHTML = "Hello!" + name + ". Your Akan Name Is " + femaleAkanNames[daysOfWeek];
 //     }
 // })
